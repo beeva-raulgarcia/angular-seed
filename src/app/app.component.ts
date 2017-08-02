bdc21e0c45163064a4cc1b7c151ad5c2301197fc
raul.garcia <raul.garcia@MacBook-Pro-de-beeva.local>
2017-08-02 10:10:19 +0200

    testing lint-staged 9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'appaaas'
+  title = 'appaasa sasaas   a'
       qq= "aa    "
 }
